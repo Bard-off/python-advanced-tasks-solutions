392ec4a92f55e06dd6c2cc12eccab1385c22e5a4
Suren Khorenyan <surenkhorenyan@gmail.com>
2024-10-23 14:02:31 +0300

    task_10 register API views

--- task_10/fastapi-users-data-app/api/api_v1/__init__.py
+++ task_10/fastapi-users-data-app/api/api_v1/__init__.py
@@ -2,6 +2,13 @@ from fastapi import APIRouter
 
 from core.config import settings
 
+from .posts import router as posts_router
+from .user_posts import router as user_posts_router
+from .users import router as users_router
+
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
+router.include_router(users_router, tags=["Users"])
+router.include_router(posts_router, tags=["Posts"])
+router.include_router(user_posts_router, tags=["Posts"])
